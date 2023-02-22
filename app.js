e09c61a0992977d4f6d7851a2ab084f26df20cf9
buikimquangloc <118284013+buikimquangloc@users.noreply.github.com>
2023-02-22 22:45:45 +0700

    modified:   app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
+
 
 const app = express();
 
@@ -8,6 +11,17 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
+});
+
+app.use("/api/contacts", contactsRouter);
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
 });
 
 module.exports = app;
